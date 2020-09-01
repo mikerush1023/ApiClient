535515e1fb326689155b94ae52317b92de8d4924
Michael Rushing <mikerush1023@gmail.com>
2020-09-01 16:50:41 -0400

    Print the table to the console

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using ConsoleTables;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Joke
     {
-      Console.WriteLine("Welcome to C#");
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+        [JsonPropertyName("setup")]
+        public string Setup { get; set; }
+        [JsonPropertyName("punchline")]
+        public string Punchline { get; set; }
     }
-  }
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/random_ten");
+
+            List<Joke> jokes = await JsonSerializer.DeserializeAsync<List<Joke>>(responseAsStream);
+
+            var table = new ConsoleTable("Id", "Type", "Setup", "Punchline");
+
+            foreach (var joke in jokes)
+            {
+                table.AddRow(joke.Id, joke.Type, joke.Setup, joke.Punchline);
+            }
+
+            table.Write();
+
+        }
+    }
 }
